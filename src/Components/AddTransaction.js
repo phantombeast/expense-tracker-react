9a85aab714a4197cb61e9314338847c586199fa4
phantombeast <rajpalpulkit12@gmail.com>
2020-11-14 23:58:10 +0530

    add-transaction

--- src/Components/AddTransaction.js
+++ src/Components/AddTransaction.js
@@ -1,10 +1,12 @@
 import React, {useState, useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
   const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
 
-
+  const { addTransaction } = useContext(GlobalContext);
+  
   const onSubmit = e => {
     e.preventDefault();
 
